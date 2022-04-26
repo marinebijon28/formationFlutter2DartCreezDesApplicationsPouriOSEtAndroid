e67404fe2d5a358afab08b0206fc0f7f82ef0580
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2022-04-26 15:44:17 +0200

    image network
    image network

--- Section9BasicWidgets/67/image_network/lib/main.dart
+++ Section9BasicWidgets/67/image_network/lib/main.dart
@@ -1,3 +1,15 @@
+/**
+ * Documentation
+ * Quand on cliaue sur n'importe laquelle des éléments. On va voir notre
+ * constructeur aussi. Si par exemple je cliquais sur Container. On n'a tous les
+ * pas besoin forcément sur la documentation. J'aime bien aller sur la
+ * documentation. J'aime bien aller sur la documentation et amener à chercher.
+ * On va pas le faire à tous les Widgets. Bien évidemment on va le faire pour
+ * les basics mais comme ça vous saurez où chercher. Là si vous cliquez dessus
+ * aue vous allez pouvoir trouver tous les paramètres et tous ce qui va pouvoir
+ * être modifié.
+ */
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -155,7 +167,7 @@ class BasicPages extends StatelessWidget {
            * Widget qui centre un autre widget tout simpement. Ici je vais vouloir
            * mon text child.
            */
-            child: Center (
+          child: Center (
             /**
              * Il aura un child demo qui sera un spanDemo. Vous vous voyez que
              * notre center est centré. Il est centré un peu bas, il est centré
@@ -178,50 +190,128 @@ class BasicPages extends StatelessWidget {
                 color: Colors.pinkAccent,
             ),
          )*/
-          /**
-           * On va passer aux images. Qu'est-ce qu'on pourrait avoir ? On
-           * pourrait avoir cette icône à l'intérieur. Ici au lieu de montrer le
-           * Container. Je vais dans Widget catalogue dans mes basics une Icon.
-           * C'est un Matériel Design. Icon elle est déjà présente dans Matériel
-           * Design. On va avoir dans les icônes : un nom, une couleur, une
-           * taille et un semanticLabel. C'est par rapport à l'accessibilité, au
-           * personne qui sont malvoyante. On a juste à taper Icon on ouvre ou
-           * on ferme les parenthèses. Ce qui faut savoir c'est-ce que les
-           * icônes, on va leur choisir principalement une taille, une couleur
-           * et bien évidemment une icône. C'est-ce au'on va voit ici. Ces
-           * icônes on va pouvoir les ajouter des IconButton, des
-           * loatingActionButton et dans plein d'endroits. L'IconButton nous
-           * servira à avoir une icône qui sera cliquable.
-           */
-          child: Icon(
-          /**
-            * Icons
-            * Il va falloir mettre juste Icons., on a voir
-            * une liste tous les icônes. Je vais choisir Icons.house. Si je veux
-            * sauvegarde et que je montre. Ici on a une icône noir. De base, elle
-            * sera noir.
-           */
-            Icons.house,
-          /**
-            * size
-            * Elle avait une size ce sera un double. On pourrait vouloir qu'elle fasse
-            * un dixième de la largeur : size.width, les médias queries of context.
-            * Donc size.width / 10. Si je l'avais / 3. On aura quelque chose comme
-            * ça. On va mettre 5 sur notre icône.
+            /**
+             * On va passer aux images. Qu'est-ce qu'on pourrait avoir ? On
+             * pourrait avoir cette icône à l'intérieur. Ici au lieu de montrer le
+             * Container. Je vais dans Widget catalogue dans mes basics une Icon.
+             * C'est un Matériel Design. Icon elle est déjà présente dans Matériel
+             * Design. On va avoir dans les icônes : un nom, une couleur, une
+             * taille et un semanticLabel. C'est par rapport à l'accessibilité, au
+             * personne qui sont malvoyante. On a juste à taper Icon on ouvre ou
+             * on ferme les parenthèses. Ce qui faut savoir c'est-ce que les
+             * icônes, on va leur choisir principalement une taille, une couleur
+             * et bien évidemment une icône. C'est-ce au'on va voit ici. Ces
+             * icônes on va pouvoir les ajouter des IconButton, des
+             * loatingActionButton et dans plein d'endroits. L'IconButton nous
+             * servira à avoir une icône qui sera cliquable.
+             */
+           /* child: Icon(
+              /**
+               * Icons
+               * Il va falloir mettre juste Icons., on a voir
+               * une liste tous les icônes. Je vais choisir Icons.house. Si je veux
+               * sauvegarde et que je montre. Ici on a une icône noir. De base, elle
+               * sera noir.
+               */
+              Icons.house,
+              /**
+               * size
+               * Elle avait une size ce sera un double. On pourrait vouloir qu'elle fasse
+               * un dixième de la largeur : size.width, les médias queries of context.
+               * Donc size.width / 10. Si je l'avais / 3. On aura quelque chose comme
+               * ça. On va mettre 5 sur notre icône.
+               */
+              size : size.width / 5,
+              /**
+               * Colors.yellow que ce soit bien moche. Ça c'est le principal.
+               */
+              color : Colors.yellow,
+              /**
+               * On pourrait avoir semanticLabel qui sera. Ceci est une maison
+               * et ça sera pour l'accessibilité. Ça honnêtement on va peu s'en
+               * servir.
+               */
+              semanticLabel: "Ceci est une maison",
             */
-            size : size.width / 5,
-          /**
-            * Colors.yellow que ce soit bien moche. Ça c'est le principal.
-            */
-            color : Colors.yellow,
-          /**
-           *  On pourrait avoir semanticLabel qui sera. Ceci est une maison et
-           *  ça sera pour l'accessibilité. Ça honnêtement on va peu s'en
-           *  servir.
-           */
-            semanticLabel: "Ceci est une maison",
-          ),
-        )
+            /**
+             * Image.network
+             * On peut télécharger une image depuis Internet. Moi ce que j'aime
+             * bien c'est allé sur pixels.com. Pourquoi ? Par ce que ce sont des
+             * images libres de droits ce qu'on pourrait faireces prendre une
+             * image au hasard. Je vais prendre une image une vidéo de plage.
+             * Ici je peux télécharger gratuitement et on va regarder. Elle est
+             * libre de droit. Parfait je télécharge gratuitement. On peut
+             * définir un lien. Cette image on l'utilisera quand on devra
+             * télécharger. Si je reviens en arriére, je peux prendre celle-là.
+             * cette image je vais vouloir faire ouvrir l'image dans un nouvel
+             * onglet. Je vais prendre l'URL de l'image. Je vais prendre celle-
+             * là. Je vais retourner dans mon Flutter dans notre projet et à la
+             * place de l'icône. Je vais commencer à taper une Image.network().
+             * Alors, qu'est-ce sait Matthieu ? On va retourner sur Widget. Ici
+             * on a une image. Donc notre image, ils vont nous expliquer comment
+             * utiliser une image avec des contructeurs. Moi ce aue je veux
+             * c'est une image, ils vont nous expliquer une image. Moi ce aue je
+             * veux c'est une Image.network(). Alors, qu'est-ce que sait
+             * Matthieu ? On va retourner sur Widget. Ici on a une image. Donc
+             * notre image. Moi ce aue je veux c'est Image.network et
+             * Image.asset.
+             * Image.network c'est une image qui a été obtenu depuis internet.
+             * On va pouvoir choisir une image, un scale, une width, une height,
+             * une couleur de fond, un alignement et un BoxFit. C'est le BoxFit,
+             * height et width qui vont être les plus important.
+             */
+            child : Image.network(
+              /**
+               * Alors, ici src c'est la source dans donc ça va être une
+               * string. C'est quoi la string ? C'est ce que j'ai copié.
+               * J'ouvre les guillemets. Je colle mon image URL. Je lance et
+               * sur mon mon application j'ai l'image. Donc on a réussi à
+               * télécharger une image sur internet.
+               */
+              "https://images.pexels.com/photos/1756086/pexels-photo-1756086.j"
+                   + "peg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              /**
+               * colors
+               * Je pourrai lui ajouter une couleur et on va mettre une
+               * Colors.teal. Ça sert à quoi Matthieu ? Ça va nous cacher
+               * notre image. Ça on ne va pas là vouloir.
+               */
+              //color : Colors.teal,
+              /**
+               * height
+               * On pourrait avoir une height. On va la mettre de 150 et vous
+               * voyez qu'on vous voyez qu'automatiquement.
+               */
+              height : 150,
+              /**
+               * width
+               * Elle s'est resizé où on pourrait avoir width disont 300. Le
+               * width de 300 n'a pas été pris en compte parce qu'il rentre dans
+               * la hauteur.
+               */
+              width: size.width,
+              /**
+               * BoxFit
+               * Je vous ai parlé du fit qui sera un BoxFit et on pourrait avoir
+               * BoxFit.. On va voir plein de choses. BoxFit.cover ça veut dire
+               * que ça va prendre la taille définit et ça va remplir en partant
+               * du centre.
+               * On pourrait avoir BoxFit.fill, ça veut dire aue ça vous remplit
+               * le container donc la hauteur et la largeur. Ça va déformez
+               * l'image pour qu'on est toute l'image.
+               * On pourrait avoir quoi le contain. La valeur de base.
+               * On va voir le FitHeight. C'est exactement la même chose ça va
+               * prendre la taille de mon image.
+               * Le fitWidth aui va centré au cover.
+               * On peut avoir scaleDown. Ça va récupérer l'image pour qu'elle
+               * rentre en entier.
+               * On va vouloir utiliser ici le cover et on pourrait avoir width.
+               * Le size.width pour que ça prenne l'intégralité. Voilà pour l'
+               * Image.network.
+               */
+              fit : BoxFit.cover
+            )
+          )
       ),
     );
   }
